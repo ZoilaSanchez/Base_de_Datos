b9e302d807fd9cfdea74f07cafb2e1db65a796a0
ZoilaSanchez <38706142+ZoilaSanchez@users.noreply.github.com>
2020-02-14 21:06:51 -0600

    Primer modulo Usuarios

---
+++ Estetica_Liz_BD/src/estetica_liz_bd/Estetica_Liz_BD.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estetica_liz_bd;
+
+import Principal.inicio_sesion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Lopez
+ */
+public class Estetica_Liz_BD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException, SQLException {
+    inicio_sesion inici=new inicio_sesion();
+    
+    Connection connection = null;
+            try {
+                //Indicamos cual driver vamos a utilizar
+                Class.forName("com.mysql.jdbc.Driver");
+
+                //Abrimos una coneccion a la DB usando una url en la que indicamos el
+                //nombre de la base datos y como parametros enviamos el usuario y la contrasena 
+                connection = DriverManager.getConnection("jdbc:mysql://localhost?user=root&password=199810");
+                Statement seleccionarBaseDatos = connection.createStatement();
+                seleccionarBaseDatos.execute("USE bd_estetica");
+
+                //Se crea el objeto escritorio para desplegar toda la interfaz grafica
+                inici.setVisible(true);
+                System.out.println("conexion establecida");
+            } catch (ClassNotFoundException e) {
+                System.out.println("error");
+                e.printStackTrace(System.out);
+            }
+    }
+    
+}
---
+++ Estetica_Liz_BD/src/estetica_liz_bd/Metodos.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estetica_liz_bd;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Lopez
+ */
+public class Metodos {
+    private static Connection conexion;
+    private static PreparedStatement sentencia_preparada;
+    private static ResultSet resultado;
+    
+    
+    
+    
+    
+    
+    
+    
+}
