9350dad7c37b5983d3442e9be7b7eb2ab3dd5f60
ZoilaSanchez <38706142+ZoilaSanchez@users.noreply.github.com>
2020-09-24 18:17:32 -0600

    Bitacora y transaccione

--- Estetica_Liz_BD/src/conexion/Conexion.java
+++ Estetica_Liz_BD/src/conexion/Conexion.java
@@ -17,6 +17,7 @@ public class Conexion {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) throws InstantiationException, IllegalAccessException, SQLException {
 
         Conectando conectar = new Conectando();
