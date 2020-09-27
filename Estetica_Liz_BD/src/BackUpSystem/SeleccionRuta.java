dd0d3b4f342eb151c5356916de9265b95b71b499
Enriqueplato <43488491+Enriqueplato@users.noreply.github.com>
2020-09-26 18:49:48 -0600

    Add Bakcup system

---
+++ Estetica_Liz_BD/src/BackUpSystem/SeleccionRuta.java
@@ -0,0 +1,68 @@
+package BackUpSystem;
+
+import java.awt.Component;
+import java.io.File;
+import javax.swing.JFileChooser;
+
+/**
+ *
+ * @author enrique7cp@gmail.com
+ */
+public class SeleccionRuta {
+
+    JFileChooser selector = new JFileChooser(System.getProperty("user.dir"));
+    FiltrosArchivos filtrotxt;
+    FiltrosArchivos filtrolist;
+    //
+
+    public SeleccionRuta() {
+        filtrolist = new FiltrosArchivos("sql", "Archivos De SQL");
+        filtrotxt = new FiltrosArchivos("txt", "Archivos De Texto");
+//        FileNameExtensionFilter filtro = new FileNameExtensionFilter("Archivos de Base de Datos", new String[] { "sql","txt"});
+//        selector.addChoosableFileFilter(filtro);
+        selector.setAcceptAllFileFilterUsed(false);
+        selector.addChoosableFileFilter(filtrolist);
+        selector.addChoosableFileFilter(filtrotxt);
+
+
+    }
+    public void ResetFiltros(){
+        selector.resetChoosableFileFilters();
+        selector.setAcceptAllFileFilterUsed(true);
+        
+    }
+
+    public File Abrir(Component parent) {
+        selector.setDialogTitle("Abrir Query de Base de Datos");
+        File f;
+        int returnVal = selector.showOpenDialog(parent);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = selector.getSelectedFile();
+            f = file;
+            //this is where a real application would open the file.
+            //salida.append("Abriendo: " + file.getName() + "." + nuevalinea);
+        } else {
+            //salida.append("Seleccion comando cancelado por usuario." + nuevalinea);
+            return null;
+        }
+
+        return f;
+    }
+
+    public File guardar(Component parent) {
+        selector.setDialogTitle("Guardar Lista De Articulos");
+        File g;
+        int returnVal = selector.showSaveDialog(parent);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = selector.getSelectedFile();
+            //this is where a real application would save the file.
+//                    salida.append("Grabando: " + file.getName() + "." + nuevalinea);
+            g = file;
+        } else {
+//                    salida.append("Grabar comando cancelado por usuario." + nuevalinea);
+            return null;
+        }
+        return g;
+    }
+}
